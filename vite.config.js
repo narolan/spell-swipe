3b3491c17a6ad9b1ac6ef993a16f6264c7244a0e
jleus <jorgi.leus@persgroep.net>
2025-07-13 23:27:01 +0200

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,40 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/spell-swipe/',
-  server: {
-    open: true,
-    port: 3000
-  }
+    plugins: [
+        react(),
+        VitePWA({
+                registerType: 'autoUpdate',
+                manifest: {
+                    name: 'Spell Swipe',
+                    short_name: 'SpellSwipe',
+                    description: 'Is it a DnD Spell or is it a metalband?',
+                    theme_color: '#121212',
+                    background_color: '#121212',
+                    display: 'standalone',
+                    start_url: '.',
+                    icons: [
+                        {
+                            src: 'icon-192.png',
+                            sizes: '192x192',
+                            type: 'image/png'
+                        },
+                        {
+                            src: 'icon-512.png',
+                            sizes: '512x512',
+                            type: 'image/png'
+                        }
+                    ]
+                }
+            }
+        )
+    ],
+    base: '/spell-swipe/',
+    server: {
+        open: true,
+        port: 3000
+    }
 })

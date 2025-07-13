a2060eac480460a008b93b7c0c14511cdbd1eba7
jleus <jorgi.leus@persgroep.net>
2025-07-13 22:22:06 +0200

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dist/'
+  base: process.env.NODE_ENV === 'production' ? '/spell-swipe' : '/public/',
+  server: {
+    open: true,
+    port: 3000
+  }
 })

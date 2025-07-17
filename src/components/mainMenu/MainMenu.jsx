234e5610eea2045bbd19df8c6b53882c7d0adf71
jleus <jorgi.leus@persgroep.net>
2025-07-17 19:46:15 +0200

    fix layout main menu

--- src/components/mainMenu/MainMenu.jsx
+++ src/components/mainMenu/MainMenu.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './MainMenu.css'
 
-const MainMenu = ({ onStartClassic, onShowScores }) => {
+const MainMenu = ({onStartClassic, onShowScores}) => {
     return (
         <div className="mainMenu">
-            <h2 className="mainMenuTitle">DnDinder - Spell Swipe</h2>
+            <h2 className="mainMenuTitle">
+                DnDinder<br/>
+                <span className="mainMenuDash">-</span><br/>
+                Spell Swipe
+            </h2>
             <h3 className="mainMenuSubTitle">Is it a DnD Spell or is it a metalband?</h3>
             <button className="playButton" onClick={onStartClassic}>Play</button>
             <button className="playButton" onClick={onShowScores}>Personal Highscores</button>
